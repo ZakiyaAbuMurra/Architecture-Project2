8353336e727b2d9d8003646b254d27d77297e5b9
ZakiyaAbuMurra <1191636@student.birzeit.edu>
2023-06-26 15:19:39 +0300

    mux 2x1 with parameters

--- mux2x1.v
+++ mux2x1.v
@@ -1,10 +1,12 @@
-module mux21 (
-  input wire [23:0] a,
-  input wire [23:0]b,
-  input wire sel,
-  output wire [23:0]out
-);
-
-assign out = sel ? b : a;
-
-endmodule 
+module mux_2x1 #(parameter DATA_WIDTH = 32)
+    (
+     input wire [DATA_WIDTH-1:0] data0,
+     input wire [DATA_WIDTH-1:0] data1,
+     input wire sel,
+     output wire [DATA_WIDTH-1:0] out
+    );
+  
+    assign out = (sel == 1'b0) ? data0 : data1;
+  
+  endmodule
+  
