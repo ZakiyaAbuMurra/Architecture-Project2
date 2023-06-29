226b89b930131ed0db1a0e7cfa01e67f6475b55a
ZakiyaAbuMurra <1191636@student.birzeit.edu>
2023-06-29 23:00:16 +0300

    test bench control unit

--- control_unit_tb.v
+++ control_unit_tb.v
@@ -1,10 +1,13 @@
+// Code your testbench here
+// or browse Examples
 module control_tb(); 
    
 	reg [1:0] inst_type;
 	reg [4:0] inst_function;
 	reg stop_bit, zero_flag; 
 	reg [2:0] state;
-	wire ExSrc, ExS, RS2src, WB, MemR, MemW, WBdata, PCsrc, PCaddSrc1, PCaddSrc2, ALUsrc, StR, StW;
+	wire ExSrc, ExS, RS2src, WB, MemR, MemW, WBdata,  PCaddSrc1, PCaddSrc2, ALUsrc, StR, StW;
+    wire [1:0] PCsrc ; 
 	wire [3:0] ALUop; // 4 bit:: TODO: change
 	wire[2:0] next_state; 
 	
@@ -15,6 +18,7 @@ module control_tb();
 						.zero_flag(zero_flag), 
 						.ExSrc(ExSrc), 
 						.ExS(ExS), 
+                        .state(state), 
 						.RS2src(RS2src),
 						.WB(WB),
 						.MemR(MemR),
@@ -36,25 +40,40 @@ module control_tb();
 		inst_function = 0 ; 
 		stop_bit	  = 0 ; 
 		zero_flag 	  = 0 ; 
-		state 		  = 0 ; 	
+		state 		  = 2 ; 	
 	end 					
 	
 	
+// 	initial
+// 	begin 
+//       forever begin 
+//         #10	inst_type     <= $urandom_range(0,3) ; 
+//         #20 inst_function <= $urandom_range(0,3); 
+//         #30 stop_bit	  <= $urandom_range(0,1) ; 
+//         #35	zero_flag 	  <= $urandom_range(0,1) ; 
+//         #10
+//         state 		  <= $urandom_range(0,5) ; 
+//       end 
+// 	end	
+  
+  
 	initial
 	begin 
-	#3	inst_type     <= $urandom_range(0,3) ; 
-	#6	inst_function <= $urandom_range(0,15); 
-	#9	stop_bit	  <= $urandom_range(0,1) ; 
-	#12	zero_flag 	  <= $urandom_range(0,1) ; 
-	#15	state 		  <= $urandom_range(0,3) ; 
-	
+      forever begin 
+        #10	inst_type     <= 10 ; 
+        #20 inst_function <=5'b00100 ; 
+        #30 stop_bit	  <= $urandom_range(0,1) ; 
+        #35	zero_flag 	  <= $urandom_range(0,1) ; 
+        #10
+        state 		  <= $urandom_range(0,5) ; 
+      end 
 	end	
 	
 	
     initial begin 
     $dumpfile("dump.vcd"); 
     $dumpvars;
-    #1000
+    #10000
     $finish ; 
     end  
 	
