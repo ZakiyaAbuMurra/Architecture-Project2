a7534bf7adc6af5b8f5892bf0a4f06b9546562ff
ZakiyaAbuMurra <1191636@student.birzeit.edu>
2023-06-29 18:16:58 +0300

    fix the opcode size

--- alu.v
+++ alu.v
@@ -1,8 +1,8 @@
 module ALU (
     input logic [31:0] A,
     input logic [31:0] B,
-    input logic [3:0]opcode , // 4 bit opcode
-      output logic [31:0]result ,
+    input logic [2:0]opcode , // 3 bit opcode
+      :output logic [31:0]result ,
       output logic carry,
       output logic zero,
       output logic negative,
@@ -16,35 +16,29 @@ module ALU (
           overflow = 0;
   
           case (opcode)
-              4'b0000: begin // ADD
+              3'b000: begin // ADD
                   result = A + B;
                   carry = A[31] & B[31] | A[31] & ~result[31] | ~result[31] & B[31]; // Carry out
                   overflow = A[31] & B[31] & ~result[31] | ~A[31] & ~B[31] & result[31]; // Overflow for addition
               end
   
-              4'b0001: begin // SUB
+              3'b001: begin // SUB
                   result = A - B;
                   carry = B > A; // Borrow in subtraction is the carry here
                   overflow = ~A[31] & B[31] & result[31] | A[31] & ~B[31] & ~result[31]; // Overflow for subtraction
               end
   
-              4'b0010: begin // AND
+              3'b010: begin // AND
                   result = A & B;
               end
   
-              4'b0011: begin // OR
-                  result = A | B;   
-              end
+             
   
-              4'b0100: begin // XOR
-                  result = A ^ B;
-              end
-  
-              4'b0101: begin // SLL
+              3'b101: begin // SLL
                   result = A << B[4:0];
               end
   
-              4'b0110: begin // SRL
+              3'b110: begin // SRL
                   result = A >> B[4:0];
               end
   
@@ -53,7 +47,7 @@ module ALU (
               end
           endcase
   
-          zero = (result == 0);
+          zero = (result == 0);	 // checks if the result is equal to zero and assigns the result to the zero flag
           negative = result[31]; // MSB as sign bit
       end
   endmodule
